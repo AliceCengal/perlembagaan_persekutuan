b9507188b37667423ebf395b08fc09ddc6f53f27
AliceCengal <cengallut@gmail.com>
2014-07-07 23:31:38 -0500

    Added first order Markov

--- markov.scala
+++ markov.scala
@@ -17,10 +17,57 @@ class WordIterator(in: InputStream) extends Iterator[String] {
   
 }
 
+type Matrix = Array[Array[Double]]
+type    Row = Array[Double]
+
+class Markovy(trainingSet: Iterator[String]) {
+  val matrix: Matrix = Array.fill(26, 26)(0.0)
+  val startingP: Row = Array.fill(26)(0.0)
+  
+  val random = new util.Random
+  
+  trainingSet.foreach { word =>
+    startingP(word.head - 'a') += 1
+    word.toList.sliding(2).foreach { 
+      case a :: b :: Nil =>
+        matrix(a - 'a')(b - 'a') += 1
+      case _ => }
+  }
+  
+  normalize(startingP)
+  matrix.foreach { row => normalize(row) }
+  
+  private def normalize(row: Row) {
+    (1 to 25).foreach { i => row(i) = row(i) + row(i-1) }
+    (0 to 25).foreach { i => row(i) /= row.last }
+  }
+  
+  private def sample(row: Row) = {
+    val dice = random.nextDouble
+    row.indexWhere(dice < _)
+  }
+  
+  def generate(length: Int): String = {
+    val first = sample(startingP)
+    
+    val gen = (2 to length).foldLeft(List(first)) { 
+      case (prevs, _) =>
+        sample(matrix(prevs.head)) :: prevs
+    }
+    
+    gen.reverse
+       .map(_ + 'a')
+       .map(_.toChar)
+       .mkString
+  }
+  
+}
+
 val wi = new WordIterator(System.in)
 
-wi.filter(_.length > 1)
-  .map(_.toLowerCase)
-  .take(100)
-  .foreach(println(_))
+val conditioned = wi.filter(_.length > 1)
+                    .map(_.toLowerCase)
 
+val markov = new Markovy(conditioned)
+
+(1 to 100).foreach { _ => println(markov.generate(10)) }
